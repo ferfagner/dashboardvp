c2c3573cd0953160150575db693a45517bc03dfa
Fagner Fernandes <ferfagner@gmail.com>
2023-10-07 01:47:21 -0300

    Update form.tsx
    atualizando para varcel

--- src/components/Form/form.tsx
+++ src/components/Form/form.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Form.css'; // Arquivo CSS que será criado
+import './form.css'; // Arquivo CSS que será criado
 
 interface infoempresaProps {
   dadosempresa?: [dadosEmpresaProps];
