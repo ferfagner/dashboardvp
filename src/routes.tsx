8dc54507310c063df56be9705a8b759f4974436c
unknown <ferfagner@gmail.com>
2023-10-07 20:02:34 -0300

    corrigindo páginas

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,6 @@
 import { Route, BrowserRouter, Routes} from "react-router-dom";
 
 import Home from "./pages/home/home";
-import InfoVendedor from "./pages/infoVendedor/infovendedor";
 import SelectVendedor from "./pages/selectVendedor/selectVendedor";
 import Dashboard from "./pages/deshboard/dashboard";
 
@@ -13,7 +12,6 @@ export default function Rotas() {
        <BrowserRouter>
            <Routes>
            <Route path="/" element={<Home/>}  />
-           <Route path="/info/:nomeVendedor/:nomeLoja" element={<InfoVendedor/>}  />
            <Route path="/selectVendedor/:nomeLoja" element={<SelectVendedor/>}  />
            <Route path="/dashboard" element={<Dashboard/>}  />
            </Routes>
