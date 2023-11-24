2113472d19df222176216d3af14005498ce2729a
Fagner Fernandes <ferfagner@gmail.com>
2023-11-24 10:50:06 -0300

    testando nova ideia.tsx

--- src/components/mainGeral/mainGeral.tsx
+++ src/components/mainGeral/mainGeral.tsx
@@ -76,7 +76,7 @@ MRAMALHO: 19
   // Process and sort the filtered sales data
   const vendasComRelacao = filteredVendas.map(venda => ({
     ...venda,
-    relacao: ((venda.vl_total_nf - venda.vl_desconto) / venda.quantidadevenda)* diasTrabalhadosPorFuncionario[venda.loginfuncionario]
+    relacao: (venda.vl_total_nf - venda.vl_desconto) / (venda.quantidadevenda / diasTrabalhadosPorFuncionario[venda.loginfuncionario])
   }));
 
   vendasComRelacao.sort((a, b) => b.relacao - a.relacao);
