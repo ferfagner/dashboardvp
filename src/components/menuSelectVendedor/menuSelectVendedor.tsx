f0ae5dac55c52f01f64d97f90dd79d9a7f9ec938
Fagner Fernandes <ferfagner@gmail.com>
2024-01-15 16:50:50 -0300

    Atualizar o menuSelectVendedor.tsx

--- src/components/menuSelectVendedor/menuSelectVendedor.tsx
+++ src/components/menuSelectVendedor/menuSelectVendedor.tsx
@@ -45,8 +45,8 @@ export default function MenuSelectVendedor({ nomeLoja }: dadosCompletos) {
     axios.post<ApiResponse>(`${baseURL}`, {
       databasecliente: "BancoDadosCasaVieiraPorto.fdb",
       comboempresas: `${nomeLoja}`,
-      //datainicial: `2023-12-01`,
-      //datafinal: `2023-12-31`,
+      datainicial: `2023-12-01`,
+      datafinal: `2023-12-31`,
       datainicial: `${primeiroDia}`,
       datafinal: `${ultimoDia}`,
       typerel: 2
